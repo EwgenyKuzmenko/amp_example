be17bbf25720dfc150e0262eade585f1b0dda241
EwgenyKuzmenko <46458547+EwgenyKuzmenko@users.noreply.github.com>
2025-06-05 22:34:54 +0300

    chore: reduce to single static page

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  // Use a relative base path so that the app works when served from a subdirectory
-  base: './',
-});
